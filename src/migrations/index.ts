546d74cd8359823d7a6b87601142b0a32724aefa
Jeremy <jeremy@massivehat.com>
2026-03-16 21:08:31 -0700

    Update index.ts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,6 @@
-import * as migration_20250929_111647 from './20250929_111647';
-import * as migration_20260228_070114 from './20260228_070114';
-import * as migration_20260301_062904_comments_collection from './20260301_062904_comments_collection';
+import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20260228_070114 from './20260228_070114'
+// import * as migration_20260301_062904_comments_collection from './20260301_062904_comments_collection';
 
 export const migrations = [
   {
@@ -13,9 +13,9 @@ export const migrations = [
     down: migration_20260228_070114.down,
     name: '20260228_070114',
   },
-  {
-    up: migration_20260301_062904_comments_collection.up,
-    down: migration_20260301_062904_comments_collection.down,
-    name: '20260301_062904_comments_collection'
-  },
-];
+  // {
+  //   up: migration_20260301_062904_comments_collection.up,
+  //   down: migration_20260301_062904_comments_collection.down,
+  //   name: '20260301_062904_comments_collection',
+  // },
+]
