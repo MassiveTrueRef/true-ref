0dc362e5fa0caafe74691fddf89c8341c224e059
Jeremy <jeremy@massivehat.com>
2026-03-18 20:42:57 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Branch: $WORKERS_CI_BRANCH"
-
-if [[ "$WORKERS_CI_BRANCH" == "main" || "$WORKERS_CI_BRANCH" == "qa" ]]; then
-  npm run build
-else
-  echo "Branch not allowed for build. Skipping."
-fi
